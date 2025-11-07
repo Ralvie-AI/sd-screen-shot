13e017fb1005c2bd2562a698e3052a69d4f088b7
hello0zai <hello0zaizai@gmail.com>
2025-11-07 16:30:44 +0700

    feat: remove unused code

--- sd_screen_shot/screenshot.py
+++ sd_screen_shot/screenshot.py
@@ -1,22 +1,14 @@
 import logging
 import os
-import platform
 from time import sleep as  time_sleep
 import logging
-
 from datetime import datetime, time
 
-from mss import mss
 import requests
+from mss import mss
 
-PROTOCOL = "http"
-# HOST = "localhost:3323"
-HOST = "182.66.219.114:9010"
-
-HOST_TO_UPLOAD_SHOT_GET = f"{PROTOCOL}://{HOST}/web/events/screenshot?fileFormat=jpg"  
-
-MAX_RETRIES = 3
-DELAY_SECONDS = 3  # wait before retry
+os.environ.pop('HTTP_PROXY', None)
+os.environ.pop('HTTPS_PROXY', None)
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -26,7 +18,8 @@ logging.basicConfig(
 )
 
 class ScreenShot:
-    def __init__(self, server_url, user_id, start_time=time(8, 0), end_time=time(17, 0), times_per_hour=7, days=[0,1,2,3,4]):
+    def __init__(self, server_url, user_id, start_time=time(8, 0), end_time=time(17, 0), 
+                 times_per_hour=7, days=[0,1,2,3,4]):
         """
         server_url: URL to POST screenshots
         start_time, end_time: datetime.time objects (default 8:00 AM - 5:00 PM)
@@ -59,73 +52,11 @@ class ScreenShot:
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         output_file = f"{screenshot_folder}/{self.user_id}_{timestamp}.png"
 
-        print("output_file ", output_file)
-
         with mss() as sct:
             sct.shot(output=output_file)
 
-        print("is file", output_file, "=>" ,os.path.isfile(output_file))
         return output_file
     
-    def get_pre_signed_url(self):
-        result = None 
-        for attempt in range(1, MAX_RETRIES + 1):
-            try:
-                res = requests.get(HOST_TO_UPLOAD_SHOT_GET)
-                data = res.json()
-                result = data.get('data').get("preSignedUrl"), data.get('data').get("objectKey")
-                break
-            except Exception as e:
-                logging.error("[ERROR]: %s", e)
-                if attempt == MAX_RETRIES:
-                    logging.info("Failed after retries.")
-                else:
-                    time_sleep(DELAY_SECONDS)
-        return result 
-       
-    def update_object_key_file(self, url_path):
-        for attempt in range(1, MAX_RETRIES + 1):
-            try:
-                res = requests.put(url_path)
-                data = res.json()
-                print("data", data)
-                logging.info(f"Upload failed with status code: {data.get('code')}")
-                logging.info(f"Url to download: {data.get('data').get('url')}")
-                break
-            except Exception as e:
-                logging.error("[ERROR]: %s", e)
-                if attempt == MAX_RETRIES:
-                    logging.info("Failed after retries.")
-                else:
-                    time_sleep(DELAY_SECONDS)
-
-    def upload_screenshot(self, file_path, presigned_url):
-
-        try:
-            # Open the file in binary mode
-            with open(file_path, 'rb') as file_obj:
-                # Perform HTTP PUT request to upload file
-                response = requests.put(presigned_url, data=file_obj)    
-            # Check response code (200 or 204 usually means success for S3)
-            if response.status_code in [200, 204]:
-                return {
-                    "status": "SUCCESS",
-                    "message": None
-                }
-            else:
-                logging.info(f"Upload failed with status code: {response.status_code}")
-                return {
-                    "status": "ERROR",
-                    "message": f"Upload failed with status code: {response.status_code}"
-                }    
-        except Exception as e:
-            logging.info(f"Exception during upload: {str(e)}")
-            return {
-                "status": "ERROR",
-                "message": str(e)
-            }
-
-
     def run(self):
         logger.info("Screenshot scheduler started")
         while True:
@@ -133,22 +64,12 @@ class ScreenShot:
             # print("self.interval 1", self.interval)
             try:
                 if self._should_take_screenshot():
-                    capture_screenshot_data = self._take_screenshot()
-                    pre_signed_url, object_key = self.get_pre_signed_url()
-                    # print("object_key ", object_key)
-                    res = self.upload_screenshot(capture_screenshot_data, pre_signed_url)
-                    if res.get('status') == "SUCCESS":
-                        payload = {
-                            'object_key': object_key,
-                            'file_location': capture_screenshot_data
-                        }
-                        print("payload ", payload)
-                        print("server url", self.server_url)
-                        response = requests.post(self.server_url, json=payload)
-                        print("response ", response.json())
-                        logger.info(f"response => {response.json()}")  
-                        logger.info(f"result url => {object_key}")  
-                    # print("self.interval", self.interval)
+                    capture_screenshot_data = self._take_screenshot()  
+                    payload = {
+                        'file_location': capture_screenshot_data
+                    }
+                    response = requests.post(self.server_url, json=payload)
+                    logger.info(f"response => {response.json()}")  
                     time_sleep(self.interval)
                 else:
                     time_sleep(10)  # wait before checking again
