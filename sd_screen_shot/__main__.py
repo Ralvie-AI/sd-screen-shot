68ec955f2c829b5148fc60a5da8d38e8cd076a95
hello0zai <hello0zaizai@gmail.com>
2025-11-07 16:29:41 +0700

    feat: add helper function to get correct type

--- sd_screen_shot/__main__.py
+++ sd_screen_shot/__main__.py
@@ -4,14 +4,31 @@ from datetime import time
 from sd_screen_shot.screenshot import ScreenShot
 # from screenshot import ScreenShot
 
+def parse_time(value: str) -> time:
+    try:
+        hour, minute = map(int, value.split(":"))
+        return time(hour, minute)
+    except ValueError:
+        raise argparse.ArgumentTypeError(
+            f"Invalid time format: '{value}'. Use HH:MM (e.g., 09:00)"
+        )
+
+def parse_days(value):
+    try:
+        return [int(v) for v in value.split(",")]
+    except ValueError:
+        raise argparse.ArgumentTypeError("Days must be comma-separated integers (e.g. 0,1,2,3,4)")
+    
 def main():
     parser = argparse.ArgumentParser(description="Screenshot uploader")
     parser.add_argument("--server_url", required=True, help="URL to upload screenshots")
     parser.add_argument("--user_id", required=True, help="User ID for identification")
-    parser.add_argument("--start_hour", type=int, default=8, help="Start time hour")
-    parser.add_argument("--end_hour", type=int, default=17, help="End time hour")
+    parser.add_argument("--start_hour", type=parse_time, default=time(8, 0),
+                    help="Start time (HH:MM)")
+    parser.add_argument("--end_hour", type=parse_time, default=time(17, 0),
+                    help="End time (HH:MM)")
     parser.add_argument("--times_per_hour", type=int, default=7, help="Screenshots per hour")
-    parser.add_argument("--days", nargs="+", type=int, default=[0,1,2,3,4], help="Allowed weekdays (0=Mon ... 6=Sun)")
+    parser.add_argument("--days", type=parse_days, default=[0,1,2,3,4], help="Allowed weekdays (0=Mon ... 6=Sun)")
 
     args = parser.parse_args()
 
@@ -20,8 +37,8 @@ def main():
     screenshot = ScreenShot(
         server_url=args.server_url,
         user_id=args.user_id,
-        start_time=time(args.start_hour, 0),
-        end_time=time(args.end_hour, 0),
+        start_time=args.start_hour,
+        end_time=args.end_hour,
         times_per_hour=args.times_per_hour,
         days=args.days
     )
